6d1eb68eef150fe31360f69643fff1c64f616be4
Phil Metscher <philmetschermailt@gmx.de>
2022-10-05 11:07:04 +0200

    add link to team

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Home content here</h1>
-      <a>This is our team</a>
+      <Link href="/team" passHref>
+        <a>This is our team</a>
+      </Link>
     </>
   );
 }
