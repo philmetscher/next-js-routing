a6a583a3ad527fe5160b2903d1cf12de4f087b19
Phil Metscher <philmetschermailt@gmx.de>
2022-10-05 11:12:01 +0200

    link all pages in nav

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import styled from "styled-components";
+
+import StyledNavLink from "./StyledNavLink";
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyledNav>
-        <a>Home</a>
-        <a>Product</a>
-        <a>Team</a>
-        <a>Account</a>
+        <Link href="/" passHref>
+          <StyledNavLink>Home</StyledNavLink>
+        </Link>
+        <Link href="/product" passHref>
+          <StyledNavLink>Product</StyledNavLink>
+        </Link>
+        <Link href="/team" passHref>
+          <StyledNavLink>Team</StyledNavLink>
+        </Link>
+        <Link href="/account" passHref>
+          <StyledNavLink>Account</StyledNavLink>
+        </Link>
       </StyledNav>
     </StyledHeader>
   );
